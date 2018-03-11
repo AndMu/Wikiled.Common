25fa9d79ea8834932c997441b7ef93b2935d821e
Andrius Mudinas <keistokas@gmail.com>
2018-03-11 14:00:46 +0000

    Improve collection

--- src/Wikiled.Common.Tests/Extensions/CollectionExtensionsTests.cs
+++ src/Wikiled.Common.Tests/Extensions/CollectionExtensionsTests.cs
@@ -12,8 +12,16 @@ namespace Wikiled.Common.Tests.Extensions
         public void Shuffle()
         {
             var data = new[] { 1, 2, 3, 4, 5, 6 };
-            var suffled = data.Shuffle(new Random(32));
+            var suffled = data.Shuffle(new Random(32)).ToArray();
+            var suffled2 = data.Shuffle(new Random(32)).ToArray();
+            var random = new Random(32);
+            var suffled3 = data.Shuffle(random).ToArray();
             Assert.AreEqual(6, suffled.Count());
+            for (int i = 0; i < suffled.Length; i++)
+            {
+                Assert.AreEqual(suffled[i], suffled2[i]);
+                Assert.AreEqual(suffled[i], suffled3[i]);
+            }
         }
     }
 }
--- src/Wikiled.Common/Extensions/CollectionExtensions.cs
+++ src/Wikiled.Common/Extensions/CollectionExtensions.cs
@@ -6,12 +6,12 @@ namespace Wikiled.Common.Extensions
 {
     public static class CollectionExtensions
     {
-        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random rng)
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random random)
         {
             T[] elements = source.ToArray();
             for (int i = elements.Length - 1; i >= 0; i--)
             {
-                int swapIndex = rng.Next(i + 1);
+                int swapIndex = random.Next(i + 1);
                 yield return elements[swapIndex];
                 elements[swapIndex] = elements[i];
             }
