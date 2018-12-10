0f5bd90e680440cb1abecb752143e688eafcf35e
Andrius Mudinas <andrius@wikiled.com>
2018-12-10 16:50:53 +0000

    Adding logging

---
+++ src/Wikiled.Common/Logging/ApplicationLogging.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Wikiled.Common.Logging
+{
+    public class ApplicationLogging
+    {
+        public static ILoggerFactory LoggerFactory { get; set; } = new LoggerFactory();
+
+        public static ILogger CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
+
+        public static ILogger CreateLogger(string categoryName) => LoggerFactory.CreateLogger(categoryName); 
+    }
+}
