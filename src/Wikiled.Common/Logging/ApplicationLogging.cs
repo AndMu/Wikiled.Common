8b8431f3a07c983a660d19f75eef704d754763df
Andrius Mudinas <keistokas@gmail.com>
2020-06-16 22:49:31 +0100

    Upgrade packages and logging

--- src/Wikiled.Common/Logging/ApplicationLogging.cs
+++ src/Wikiled.Common/Logging/ApplicationLogging.cs
@@ -6,7 +6,7 @@ namespace Wikiled.Common.Logging
     {
         public static ILoggerFactory LoggerFactory { get; set; } = new LoggerFactory();
 
-        public static ILogger CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
+        public static ILogger<T> CreateLogger<T>() => LoggerFactory.CreateLogger<T>();
 
         public static ILogger CreateLogger(string categoryName) => LoggerFactory.CreateLogger(categoryName); 
     }
