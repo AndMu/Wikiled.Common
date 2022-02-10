9b53aed7659757b58d939ba1dbec4516b9e1dbef
Andrius Mudinas <keistokas@gmail.com>
2022-02-10 08:20:41 +0000

    Update datetime helper

--- src/Wikiled.Common.Tests/Helpers/DateTimeHelperTests.cs
+++ src/Wikiled.Common.Tests/Helpers/DateTimeHelperTests.cs
@@ -18,9 +18,43 @@ namespace Wikiled.Common.Tests.Helpers
 
         [TestCase("2012-02-02", 1328140800)]
         [TestCase("2014-02-02", 1391299200)]
-        public void FromUnixTime(string date, long seconds)
+        public void ToUnixTime(string date, long seconds)
         {
             var result = DateTime.Parse(date).ToUnixTime();
+            Assert.AreEqual(seconds, result);
+        }
+
+        [TestCase(1328140800000, "2012-02-02")]
+        [TestCase(1391299200000, "2014-02-02")]
+        [TestCase(0, "1970-01-01")]
+        public void FromUnixTimeMilis(long seconds, string date)
+        {
+            var result = seconds.FromUnixTimeMilis();
+            Assert.AreEqual(DateTime.Parse(date), result);
+        }
+
+        [TestCase("2012-02-02", 1328140800000)]
+        [TestCase("2014-02-02", 1391299200000)]
+        public void ToUnixTimeMilis(string date, long seconds)
+        {
+            var result = DateTime.Parse(date).ToUnixTimeMilis();
+            Assert.AreEqual(seconds, result);
+        }
+
+        [TestCase(15372, "2012-02-02")]
+        [TestCase(16103, "2014-02-02")]
+        [TestCase(0, "1970-01-01")]
+        public void FromUnixDays(int days, string date)
+        {
+            var result = days.FromUnixDays();
+            Assert.AreEqual(DateTime.Parse(date), result);
+        }
+
+        [TestCase("2012-02-02", 15372)]
+        [TestCase("2014-02-02", 16103)]
+        public void ToUnixDays(string date, int seconds)
+        {
+            var result = DateTime.Parse(date).ToUnixDays();
             Assert.AreEqual(seconds, result);
         }
     }
--- src/Wikiled.Common/Helpers/DateTimeHelper.cs
+++ src/Wikiled.Common/Helpers/DateTimeHelper.cs
@@ -4,17 +4,38 @@ namespace Wikiled.Common.Helpers
 {
     public static class DateTimeHelper
     {
-        private static DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 
         public static DateTime FromUnixTime(this long unixTimeStamp)
         {
             // Unix timestamp is seconds past epoch
-            return epoch.AddSeconds(unixTimeStamp).ToLocalTime();
+            return Epoch.AddSeconds(unixTimeStamp);
         }
 
         public static long ToUnixTime(this DateTime data)
         {
-            return Convert.ToInt64((data - epoch).TotalSeconds);
+            return Convert.ToInt64((data - Epoch).TotalSeconds);
+        }
+
+        public static DateTime FromUnixTimeMilis(this long unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            return Epoch.AddMilliseconds(unixTimeStamp);
+        }
+
+        public static long ToUnixTimeMilis(this DateTime data)
+        {
+            return Convert.ToInt64((data - Epoch).TotalMilliseconds);
+        }
+
+        public static DateTime FromUnixDays(this int unixTimeStamp)
+        {
+            return Epoch.AddDays(unixTimeStamp);
+        }
+
+        public static int ToUnixDays(this DateTime data)
+        {
+            return Convert.ToInt32((data - Epoch).TotalDays);
         }
     }
 }
